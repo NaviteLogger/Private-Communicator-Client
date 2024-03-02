5271b8e1ef89201e5912862ab9d3c1d5753de773
NaviteLogger <kacprzakmarek92@gmail.com>
2024-03-02 18:06:29 +0100

    Added a function responsible for the serialization of the key pair

--- client.py
+++ client.py
@@ -13,3 +13,10 @@ def generate_key_pair():
     public_key = private_key.public_key()
 
     return private_key, public_key
+
+# The following function deals with the serialization of the key pair
+def serialize_public_key(public_key):
+    return public_key.public_bytes(
+        encoding = serialization.Encoding.PEM,
+        format = serialization.PublicFormat.SubjectPublicKeyInfo
+    )
