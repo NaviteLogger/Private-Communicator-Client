115bafcf9871b11fd76786b30cfa61c6fee9ff08
NaviteLogger <kacprzakmarek92@gmail.com>
2024-03-02 18:01:47 +0100

    Added a function responsible for the generation of key pair

---
+++ client.py
@@ -0,0 +1,15 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+
+# The following function deals with the generation of a key pair
+def generate_key_pair():
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend()
+    )
+
+    public_key = private_key.public_key()
+
+    return private_key, public_key
